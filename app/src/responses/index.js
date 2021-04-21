b0b228ec2fbbb65ffc7126f94932436c0219355c
fjij <30779570+fjij@users.noreply.github.com>
2021-04-21 13:32:54 -0400

    SMH

--- app/src/responses/index.js
+++ app/src/responses/index.js
@@ -1,25 +1,19 @@
 export function messageResponse(content) {
   return {
-    content,
     type: 4,
-    tts: false,
-    embeds: [],
-    allowed_mentions: {
-      parse: []
+    data: {
+      content
     }
   }
 }
 
 export function embedResponse(embed) {
   return {
-    content: "",
     type: 4,
-    tts: false,
-    embeds: [
-      embed
-    ],
-    allowed_mentions: {
-      parse: []
+    data: {
+      embeds: [
+        embed
+      ]
     }
   }
 }
