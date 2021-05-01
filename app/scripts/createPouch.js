fe3877b243919effc391da5858b01caacdaba0f1
fjij <30779570+fjij@users.noreply.github.com>
2021-05-01 17:21:17 -0400

    Update scripts to use token names and tickers

--- app/scripts/createPouch.js
+++ app/scripts/createPouch.js
@@ -1,9 +1,21 @@
 import { connect } from '../src/db/index.js';
+import { getTokenByTicker } from '../src/tokens/index.js';
 import { Pouch } from '../src/pouch/index.js';
+import * as eth2 from '../src/eth2/index.js';
+import { Amount } from '../src/eth2/amount.js';
 
-async function createPouch(userId, tokenId, balance) {
+async function createPouch(userId, ticker, value) {
   await connect();
-  const pouch = await (new Pouch({ userId, tokenId, balance })).save();
+  await eth2.init();
+  const token = await getTokenByTicker(ticker);
+  const amount = Amount.fromStringValue(token, value);
+
+  const pouch = await (new Pouch({
+    userId,
+    tokenId: token.id,
+    balance: amount.getValue().toString()
+  })).save();
+
   console.dir({
     id: pouch.id,
     userId: pouch.userId,
@@ -13,16 +25,16 @@ async function createPouch(userId, tokenId, balance) {
 }
 
 const userId = process.argv[2];
-const tokenId = process.argv[3];
-const balance = process.argv[4];
-if (userId && tokenId && balance) {
-  createPouch(userId, tokenId, balance).then(() => {
+const ticker = process.argv[3];
+const amount = process.argv[4];
+if (userId && ticker && amount) {
+  createPouch(userId, ticker, amount).then(() => {
     process.exit(0);
   }).catch(e => {
     console.error(e);
     process.exit(1);
   });
 } else {
-  console.error('Must provide arguments: userId tokenId balance');
+  console.error('Must provide arguments: userId ticker amount');
   process.exit(1);
 }
